67f708e5eda1771dae6eb274f4a476b93fd1f57d
Documenter.jl <documenter@juliadocs.github.io>
2021-02-27 14:02:43 +0000

    build based on 62a00e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = COPInstances","category":"page"},{"location":"#COPInstances","page":"Home","title":"COPInstances","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [COPInstances]","category":"page"}]
+}
